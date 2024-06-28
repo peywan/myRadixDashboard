ea4c5b72e2f2f9462398208747e2aea9c6b78abe
Peywan Sabir <peywan1999@icloud.com>
2024-06-28 13:26:18 +0200

    22

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
-import './App.css'
+// src/App.jsx
+import React from 'react';
+import Menubar from '@/components/ui/menubar';
+import Container from '@/components/Container';
+import OtherComponent from '@/components/ui/OtherComponent'; // Example component
 
 function App() {
-
-  return (
-    <>
-    </>
-  )
+    return (
+        <Container>
+            <Menubar />
+            <div className="flex flex-col md:flex-row space-y-4 md:space-y-0 md:space-x-4">
+                <div className="w-full md:w-1/3">
+                    <OtherComponent />
+                </div>
+                <div className="w-full md:w-2/3">
+                    {/* Another component */}
+                </div>
+            </div>
+        </Container>
+    );
 }
 
-export default App
+export default App;
